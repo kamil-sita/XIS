709af3dd7beb9f24fa0813872cfbf655c9fc020b
Sita <gtnetr@gmail.com>
2018-09-01 14:31:52 +0200

    readded file

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sections.main.MainViewController;
-import sections.scannertonote.ScannerToNoteConverter;
 
 public final class Main extends Application {
 

012b59be0c4de4193904bf8fd5da0d15fd90cd10
Sita <gtnetr@gmail.com>
2019-02-28 13:39:35 +0100

    added docs. Default page will attempt to load docs from github.io page

--- src/main/java/sections/defaultpage/WelcomePageController.java
+++ src/main/java/sections/defaultpage/WelcomePageController.java
@@ -1,7 +1,37 @@
 package sections.defaultpage;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import sections.UserFeedback;
 import sections.XisController;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public final class WelcomePageController extends XisController {
 
+    @FXML
+    private WebView webView;
+
+    @FXML
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                URL url = new URL("http://kamil-sita.github.io/xis");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                WebEngine engine = webView.getEngine();
+                Platform.runLater(() -> engine.load(url.toString()));
+            } catch (Exception e) {
+                UserFeedback.getInstance().reportProgress("Could not load webpage");
+            }
+        }).start();
+        registerNotifier((width, height) -> {
+           webView.setPrefHeight(height - 100);
+           webView.setMinHeight(height - 100);
+        });
+    }
 }

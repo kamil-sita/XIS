21b175173ed3ab2032e5fd99414d2557e7d2dbc4
sita <gtnetr@gmail.com>
2019-06-19 17:40:50 +0200

    removed thread locking at it caused major problems

--- src/main/java/XIS/sections/SingleJobManager.java
+++ src/main/java/XIS/sections/SingleJobManager.java
@@ -1,7 +1,5 @@
 package XIS.sections;
 
-import java.util.concurrent.Semaphore;
-
 /**
  * Manages background operation that take time and attempts to make sure only one such operation exists. If a job
  * is interrupted before finishing its onUninterruptedFinish() method won't be called.
@@ -11,14 +9,7 @@ public final class SingleJobManager {
     private static Interruptible currentJob = null;
     private static UserFeedback userFeedback;
 
-    private static Semaphore threadAccess = new Semaphore(1);
-
     public static void setAndRunJob(Interruptible job) {
-        try {
-            threadAccess.acquire();
-        } catch (InterruptedException e) {
-            return;
-        }
         attemptInterrupt();
         currentJob = job;
         new Thread(() -> {
@@ -27,22 +18,15 @@ public final class SingleJobManager {
                 job.onUninterruptedFinish().run();
             }
         }).start();
-        threadAccess.release();
     }
 
     public static void attemptInterrupt() {
-        try {
-            threadAccess.acquire();
-        } catch (InterruptedException e) {
-            return;
-        }
         if (currentJob != null) {
             currentJob.interrupt();
             currentJob = null;
             if (userFeedback == null) userFeedback = new UserFeedback();
             userFeedback.reportProgress(0);
         }
-        threadAccess.release();
     }
 
 }
